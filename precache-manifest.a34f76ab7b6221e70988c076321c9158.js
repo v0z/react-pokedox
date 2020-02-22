7aca39a62d9e158d29806bc18cfd39bc4e29afda
Vasya M <tangerinedevourer@gmail.com>
2020-02-22 16:18:18 +0200

    Updates

---
+++ precache-manifest.a34f76ab7b6221e70988c076321c9158.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e4b5c46641ec029fe5d6147b2a8c9c5",
+    "url": "/react-pokedox/index.html"
+  },
+  {
+    "revision": "2b8753d5429c7ea122ed",
+    "url": "/react-pokedox/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "d90b216b079b8298b95c",
+    "url": "/react-pokedox/static/css/main.f828fb5c.chunk.css"
+  },
+  {
+    "revision": "2b8753d5429c7ea122ed",
+    "url": "/react-pokedox/static/js/2.ed6ae04f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-pokedox/static/js/2.ed6ae04f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d90b216b079b8298b95c",
+    "url": "/react-pokedox/static/js/main.8153a931.chunk.js"
+  },
+  {
+    "revision": "a718cf561d69d4dadfdb",
+    "url": "/react-pokedox/static/js/runtime-main.3d5569df.js"
+  }
+]);
